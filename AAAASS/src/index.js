1c273725057f5a6364e2e8fcacaf326193e0b2ae
wintermelyn <matas.seplveda@uc.cl>
2024-06-25 22:33:06 -0400

    d

--- AAAASS/src/index.js
+++ AAAASS/src/index.js
@@ -8,7 +8,7 @@ const httpsOptions = {
     cert: fs.readFileSync('cert1.pem')
 };
 const server = https.createServer(httpsOptions);
-const port = 80;
+const port = 8090;
 const clients = {};
 const wss = new WebSocketServer({ server });
 
